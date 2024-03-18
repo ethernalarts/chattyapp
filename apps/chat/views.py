84068f85ce00f32b298a268d9d3ffd868d1818d6
ethernalarts <51105148+ethernalarts@users.noreply.github.com>
2024-03-18 16:59:31 +0100

    added register.html

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -2,14 +2,16 @@ from django.contrib.auth import logout
 
 # from django.shortcuts import render
 from django.contrib import messages
-from django.urls import reverse
-from django.views.generic import TemplateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
+from apps.users.forms import CreateAccountForm
 
 
 @login_required
@@ -24,11 +26,13 @@ class ChatRoom(TemplateView):
 class ChatLoginView(LoginView):
     redirect_authenticated_user = True
     redirect_field_name = "next"
-    success_url = "chatroom.html"
+    success_url = reverse_lazy("chat:chat-room")
+    template_name = "login.html"
     fields = "__all__"
 
     def post(self, request, *args, **kwargs):
         form = self.get_form()
+
         if form.is_valid():
             messages.success(
                 self.request, f"You are logged in as {str(form.get_user()).upper()}"
@@ -57,3 +61,23 @@ class ChatLogoutView(LogoutView):
         if redirect_to != request.get_full_path():
             return HttpResponseRedirect(redirect_to)
         return super().get(request, *args, **kwargs)
+
+
+class CreateAccountView(CreateView):
+    form_class = CreateAccountForm
+    success_url = reverse_lazy("login")
+    template_name = 'register.html'
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+
+        try:
+            # if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(self.request, f'Account has been created for {username}!')
+            return self.form_valid(form)
+        except Exception as e:
+            messages.error(self.request, f'{e}')
+            return self.form_invalid(form)
+            # return render(request, "register.html", {'form': form})
